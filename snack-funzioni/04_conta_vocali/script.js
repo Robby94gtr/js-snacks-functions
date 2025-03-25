bddb5ef019bd8f67c4175a5cb116761ee1909376
Robby94gtr <robertogramazio94@gmail.com>
2025-03-25 20:25:24 +0100

    [UPD] done 04_conta_vocali

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,9 +4,21 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(string){
+    const vowels = ["a", "e", "i", "o", "u"];
+    let count = 0;
 
+    for(let i=0; i<string.length; i++){
+        if(vowels.includes(string[i])){
+
+            count ++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(countVowels(word));
 
 
 
