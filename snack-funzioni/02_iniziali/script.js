82bfeb99b30821597d250438cf4d0d12b508dba9
Robby94gtr <robertogramazio94@gmail.com>
2025-03-25 20:01:15 +0100

    [UPD] Done 02_iniziali

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -5,9 +5,23 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function getInitials(array){
+    const initials = [];
+
+    for(let i=0; i<array.length; i++){
+        let name = array[i];
+        let firstLetter = name[0];
+        
+        initials.push(firstLetter);
+    }
+
+    return initials
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getInitials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
