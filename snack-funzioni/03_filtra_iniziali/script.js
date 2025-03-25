8708f4efbe00d4a0357aa14ad2c6fc89751e3605
Robby94gtr <robertogramazio94@gmail.com>
2025-03-25 20:15:03 +0100

    [UPD] Done 03_filtra_iniziali

--- snack-funzioni/03_filtra_iniziali/script.js
+++ snack-funzioni/03_filtra_iniziali/script.js
@@ -4,9 +4,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+let filterByInitial = (array, letter) => {
+    const filtered = [];
+
+    for(let i=0; i<array.length; i++){
+
+        if(array[i][0].toLowerCase() === letter.toLowerCase()){
+            filtered.push(array[i]);
+        }
+    }
+
+    return filtered;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(filterByInitial(names, "a"));
 
 
 
